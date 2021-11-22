fb78353ad7199dd671e97d57424bfdfc30724e79
Henrique Correa do Amaral <henriqueamaralbh13@gmail.com>
2021-11-22 14:16:04 -0300

    dotenv updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,7 +10,10 @@ import { TeacherController } from '../teacher/teacher.controller';
 import { TeacherService } from '../teacher/teacher.service';
 
 @Module({
-  imports: [ConfigModule.forRoot(), MongooseModule.forRoot(`mongodb+srv://${process.env.DBLOGIN}:${process.env.DBPASS}@nest-app.38kvt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`), MongooseModule.forFeature([{ name: Student.name, schema: StudentSchema }, { name: Teacher.name, schema: TeacherSchema }])],
+  imports: [
+    ConfigModule.forRoot(), 
+    MongooseModule.forRoot(process.env.MONGODBLINK), 
+    MongooseModule.forFeature([{ name: Student.name, schema: StudentSchema }, { name: Teacher.name, schema: TeacherSchema }])],
   controllers: [StudentController, TeacherController, StudentTeacherController],
   providers: [StudentService, TeacherService],
 })
